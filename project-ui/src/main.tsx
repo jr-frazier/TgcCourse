6989151d440eacec37b83ea7cb85addb1f8a1cfb
Justin Frazier <justinfrazier@justins-mbp-2.lan>
2022-08-01 16:53:16 -0400

    upgraded to ts and added UI

--- project-ui/src/main.tsx
+++ project-ui/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  BrowserRouter,
+  Route
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </BrowserRouter>
 )
